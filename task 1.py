70968b62bd956ab4aa8d74583fa9e81aa8623239
Niyaz Khasanov <khasanchik85@gmail.com>
2024-08-24 02:13:10 +0500

    Был исправлен код под условия задачи

--- task 1.py
+++ task 1.py
@@ -1,16 +1,18 @@
 import threading
 
+# Переменные для хранения значений
+max_value = None
+min_value = None
+
 
 def find_max(numbers):
-    """Функция для нахождения максимального значения в списке чисел.Параметры:
+    """Функция для нахождения максимального значения в списке чисел.
+    Параметры:
     numbers (list): Список чисел, в котором будет производиться поиск максимума.
     Возвращает:
-    None: Результат выводится на экран.
-    Выполняется 5 раз, каждый раз выводя текущее максимальное значение."""
-
-    for i in range(5):
-        max_value = max(numbers)
-        print(f"Максимум: {max_value}")
+    None"""
+    global max_value
+    max_value = max(numbers)  # Находим максимум
 
 
 def find_min(numbers):
@@ -18,31 +20,36 @@ def find_min(numbers):
     Параметры:
     numbers (list): Список чисел, в котором будет производиться поиск минимума.
     Возвращает:
-    None: Результат выводится на экран.
-    Выполняется 5 раз, каждый раз выводя текущее минимальное значение."""
-
-    for i in range(5):
-        min_value = min(numbers)
-        print(f"Минимум: {min_value}")
+    None"""
+    global min_value
+    min_value = min(numbers)  # Находим минимум
 
 
 # Запросить у пользователя ввод значений для списка
 while True:
     user_input = input("Введите числа через пробел: ")
     try:
-        numbers_list = list(map(float, user_input.split()))
+        numbers_list = list(map(int, user_input.split()))
         break  # Выйти из цикла, если ввод корректен
     except ValueError:
         print("Пожалуйста, введите только числовые значения.")
 
 # Создание потоков
-max_thread = threading.Thread(target=find_max, args=(numbers_list,))
-min_thread = threading.Thread(target=find_min, args=(numbers_list,))
+thread_1 = threading.Thread(target=find_max, args=(numbers_list,))
+thread_2 = threading.Thread(target=find_min, args=(numbers_list,))
 
 # Запуск потоков
-max_thread.start()
-min_thread.start()
+thread_1.start()
+thread_2.start()
 
 # Ожидание завершения потоков
-max_thread.join()
-min_thread.join()
+thread_1.join()
+thread_2.join()
+
+# Цикл для вывода значений поочередно
+for _ in range(5):
+    print(f"{max_value}/{min_value}", end="")
+    if _ < 4:
+        print("/", end="")
+
+print()
